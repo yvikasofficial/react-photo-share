cfa9e7700cbef6d298f96346d1df0dab7251d653
JyotikaKoneru <125540355+JyotikaKoneru@users.noreply.github.com>
2023-12-05 09:00:23 -0500

    Adding schema folder files

---
+++ schema/user.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var mongoose = require("mongoose");
+
+/**
+ * Define the Mongoose Schema for a Comment.
+ */
+const userSchema = new mongoose.Schema({
+  first_name: String,
+  last_name: String,
+  location: String,
+  description: String,
+  occupation: String,
+  login_name: String,
+  password: String
+});
+
+/**
+ * Create a Mongoose Model for a User using the userSchema.
+ */
+const User = mongoose.model("User", userSchema);
+
+/**
+ * Make this available to our application.
+ */
+module.exports = User;
+
+
